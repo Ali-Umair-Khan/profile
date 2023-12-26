499a4c4f7d14f4aa41f70161775288ece1a2a608
Ali-Umair-Khan <rexexpert@gmail.com>
2023-12-26 18:35:57 +0500

    changes in skills.js

--- src/app/components/skills/skills.js
+++ src/app/components/skills/skills.js
@@ -4,8 +4,7 @@ const Skills = () => {
     return(
         <section className='skills'>
             <h1 className='skills__h1'>
-                "Pixels are my playground. Specialized in the realms of mobile and web app design, 
-                I thrive on crafting seamless digital experiences."
+            &quot;Pixels are my playground. Specialized in the realms of mobile and web app design, I thrive on crafting seamless digital experiences.&quot;
             </h1>
             <p className='skills__p'>Alongside, I have a knack for designing and building websites, turning concepts into captivating online realities.</p>
         </section>
