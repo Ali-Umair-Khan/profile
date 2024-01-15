ffe67fdeb33d7aeb442d6c37a4cc11fbc3fd5731
Ali-Umair-Khan <rexexpert@gmail.com>
2024-01-15 15:28:31 +0500

    changes in text

--- src/app/components/contact/contact.jsx
+++ src/app/components/contact/contact.jsx
@@ -21,9 +21,10 @@ const Contact = () => {
     return(
         <section className='contact'>
             <h1 className='contact__h1'>
-            My tech stack is MERN/Next. I am actively looking for collaboration on challenges related to these techs, particularly those found on platforms. 
+            My stack is MERN/Next. I am actively looking for collaboration on challenges related to this tech, particularly those found on platforms. 
             I am also open to joining projects focusing on the meticulous development of web apps, sophisticated landing pages, and websites. 
-            If you share similar interests, I am eager to explore potential collaborative opportunities.
+            I usually follow component based approach, where each section/component of webpage is provided with separate folders having separate files for
+            code and styling, I style using sass. If you share similar interests, I am eager to explore potential collaborative opportunities.
             </h1>
             <ul className='contact__socials'>
                 <a href={X__link} target="_blank" rel="noopener noreferrer"><img src='/banner/twitter4.png' alt='twitter'/></a>
